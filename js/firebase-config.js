77508e90d2112e43193b3713f1c4bd372e731121
Ahmed Raza Ur Rehman. <222101005@ist.edu.pk>
2026-04-21 20:29:38 +0500

    ghgjkjhgff

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: 'replace-me',
-  authDomain: 'replace-me.firebaseapp.com',
-  projectId: 'replace-me',
-  storageBucket: 'replace-me.appspot.com',
-  messagingSenderId: 'replace-me',
-  appId: 'replace-me'
-};
